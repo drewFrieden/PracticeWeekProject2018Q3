fc8a70f390b4689d6906409a6e93a157b43db946
Drew Frieden <drew.frieden@buildium.com>
2018-09-11 13:25:31 -0400

    First Commit

---
+++ train_simple_nn.py
@@ -0,0 +1,19 @@
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+ 
+# import the necessary packages
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import SGD
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+import pickle
+import cv2
+import os
