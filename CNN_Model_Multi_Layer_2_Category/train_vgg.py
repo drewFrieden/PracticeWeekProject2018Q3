03d6f58a4f2d3a9ce785406a8336e73cb474e4c9
Drew Frieden <drew.frieden@buildium.com>
2018-09-13 11:57:02 -0400

    Moved CNN to a separate folder + Doing Additional cleanup

--- CNN_Model_Multi_Layer_2_Category/train_vgg.py
+++ CNN_Model_Multi_Layer_2_Category/train_vgg.py
@@ -3,13 +3,14 @@ import matplotlib
 matplotlib.use("Agg")
  
 # import the necessary packages
+from pyimagesearch.smallvggnet import SmallVGGNet
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from keras.models import Sequential
-from keras.layers.core import Dense
+from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import SGD
 from imutils import paths
+from keras.utils import to_categorical
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
@@ -17,6 +18,7 @@ import random
 import pickle
 import cv2
 import os
+from sklearn.metrics import accuracy_score
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -24,8 +26,6 @@ ap.add_argument("-d", "--dataset", required=True,
 	help="path to input dataset of images")
 ap.add_argument("-m", "--model", required=True,
 	help="path to output trained model")
-ap.add_argument("-l", "--label-bin", required=True,
-	help="path to output label binarizer")
 ap.add_argument("-p", "--plot", required=True,
 	help="path to output accuracy/loss plot")
 args = vars(ap.parse_args())
@@ -42,11 +42,11 @@ random.shuffle(imagePaths)
  
 # loop over the input images
 for imagePath in imagePaths:
-	# load the image, resize the image to be 32x32 pixels (ignoring
-	# aspect ratio), flatten the image into 32x32x3=3072 pixel image
-	# into a list, and store the image in the data list
+	# load the image, resize it to 64x64 pixels (the required input
+	# spatial dimensions of SmallVGGNet), and store the image in the
+	# data list
 	image = cv2.imread(imagePath)
-	image = cv2.resize(image, (32, 32)).flatten()
+	image = cv2.resize(image, (128, 128))
 	data.append(image)
  
 	# extract the class label from the image path and update the
@@ -71,34 +71,54 @@ lb = LabelBinarizer()
 trainY = lb.fit_transform(trainY)
 testY = lb.transform(testY)
 
-# define the 3072-1024-512-3 architecture using Keras
-model = Sequential()
-model.add(Dense(1024, input_shape=(3072,), activation="sigmoid"))
-model.add(Dense(512, activation="sigmoid"))
-model.add(Dense(len(lb.classes_), activation="softmax"))
-
-# initialize our initial learning rate and # of epochs to train for
-INIT_LR = 0.01
-EPOCHS = 75
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+(trainX, testX, trainY, testY) = train_test_split(data,
+	labels, test_size=0.25, random_state=42)
  
-# compilg the model using SGD as our optimizer and categorical
-# cross-entropy loss (you'll want to use binary_crossentropy
-# for 2-class classification)
+# convert the labels from integers to vectors (for 2-class, binary
+# classification you should use Keras' to_categorical function
+# instead as the scikit-learn's LabelBinarizer will not return a
+# vector)
+lb = LabelBinarizer()
+trainY = to_categorical(trainY)
+testY = to_categorical(testY)
+
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
+	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+	horizontal_flip=True, fill_mode="nearest")
+ 
+# initialize our VGG-like Convolutional Neural Network
+model = SmallVGGNet.build(width=128, height=128, depth=3,
+	classes=2)
+
+# initialize our initial learning rate, # of epochs to train for,
+# and batch size
+INIT_LR = 0.01
+EPOCHS = 1
+BS = 32
+ 
+# initialize the model and optimizer (you'll want to use
+# binary_crossentropy for 2-class classification)
 print("[INFO] training network...")
-opt = SGD(lr=INIT_LR)
+opt = SGD(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="categorical_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
-# train the neural network
-H = model.fit(trainX, trainY, validation_data=(testX, testY),
-	epochs=EPOCHS, batch_size=32)
+# train the network
+H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
+	validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
+	epochs=EPOCHS)
 
 # evaluate the network
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=32)
 print(classification_report(testY.argmax(axis=1),
-	predictions.argmax(axis=1), target_names=lb.classes_))
- 
+	predictions.argmax(axis=1), target_names=["form","pic"]))
+print(accuracy_score(testY.argmax(axis=1),
+	predictions.argmax(axis=1)))
+
 # plot the training loss and accuracy
 N = np.arange(0, EPOCHS)
 plt.style.use("ggplot")
@@ -107,15 +127,15 @@ plt.plot(N, H.history["loss"], label="train_loss")
 plt.plot(N, H.history["val_loss"], label="val_loss")
 plt.plot(N, H.history["acc"], label="train_acc")
 plt.plot(N, H.history["val_acc"], label="val_acc")
-plt.title("Training Loss and Accuracy (Simple NN)")
+plt.title("Training Loss and Accuracy (SmallVGGNet)")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend()
 plt.savefig(args["plot"])
-
+ 
 # save the model and label binarizer to disk
 print("[INFO] serializing network and label binarizer...")
-model.save(args["model"])
-f = open(args["label_bin"], "wb")
-f.write(pickle.dumps(lb))
-f.close()
+#model.save(args["model"])
+#f = open(args["label_bin"], "wb")
+#f.write(pickle.dumps(lb))
+#f.close
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # PracticeWeekProject2018Q3
 A machine learning project for Q3 2018 at buildium
 
+Folders:
+
+ImageDownload - A project for scraping images from google search.
+Notebooks - Some example utility notebooks as well as our final presentation notebook.
+CNN_Model_Multi_Layer_2_Category - A Convultional Neural Network project that uses two categories of images to create a model.
+
 Example run:
 
-python .\train_simple_nn.py --dataset 'C:\Users\drew.frieden\Documents\PracticeWeekProject2018Q3\forms'  --model ../model --label-bin ../model --plot ../images
+python .\train_vgg.py --dataset '..\PracticeWeekProject2018Q3\CNN_Model_Multi_Layer_2_Category\Test_Data' --model "..\PracticeWeekProject2018Q3\CNN_Model_Multi_Layer_2_Category\Model" --plot
+'..\PracticeWeekProject2018Q3\CNN_Model_Multi_Layer_2_Category\Output'
