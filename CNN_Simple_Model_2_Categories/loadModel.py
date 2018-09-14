b75394d80cfbe1179d90875124da8ed15b5212c9
Alla Hoffman <alla.hoffman@buildium.com>
2018-09-14 14:46:36 -0400

    cleaned up loadModel and README

--- CNN_Simple_Model_2_Categories/loadModel.py
+++ CNN_Simple_Model_2_Categories/loadModel.py
@@ -6,12 +6,12 @@ from keras.optimizers import SGD
 
 model = load_model('.\model\model.h5')
 
-#picturePath = '.\model\\doc.jpg'
+#replace this picture path with the path to any image
+#on your system that you'd like the model to classify
 picturePath = '.\model\\facepalm.png'
 test_image = cv2.imread(picturePath)
 output = test_image.copy()
 test_image = cv2.resize(test_image, (32, 32)).flatten()
 test_image = test_image.reshape((1, test_image.shape[0]))
-print(np.array(test_image).shape)
 result = model.predict(np.array(test_image), batch_size=1, verbose=1)   
 print(result)
